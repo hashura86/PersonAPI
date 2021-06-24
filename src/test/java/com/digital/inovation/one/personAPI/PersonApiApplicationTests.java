a56df192d8abb4d748489a3ab510f0a384f1dbfe
joão ribeiro <joaoribeiro86@hotmail.com>
2021-06-24 11:32:21 -0300

    projeto da DIO

---
+++ src/test/java/com/digital/inovation/one/personAPI/PersonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.inovation.one.personAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
